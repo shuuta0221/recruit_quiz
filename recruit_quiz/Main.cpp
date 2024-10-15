628138983df8fb337906d18ce8620ae29fa54731
しゅうた <takumi.onoe@gmail.com>
2024-10-15 17:50:46 +0900

    問題文の末尾を修正

--- recruit_quiz/Main.cpp
+++ recruit_quiz/Main.cpp
@@ -20,13 +20,13 @@ int main() {
 	//掛け算
 	int x = uniform_int_distribution<>(1, 30)(rand);
 	int y = uniform_int_distribution<>(1, 20)(rand);
-	questions[0].s = to_string(x) + "x" + to_string(y);
+	questions[0].s = to_string(x) + "x" + to_string(y) + "の答えは？";
 	questions[0].a = x * y;
 
 	//割り算
 	x = uniform_int_distribution<>(1, 30)(rand);
 	y = uniform_int_distribution<>(1, 20)(rand);
-	questions[1].s = to_string(x * y) + "÷" + to_string(y);
+	questions[1].s = to_string(x * y) + "÷" + to_string(y) + "の答えは？";
 	questions[1].a = x;
 
 	//複雑な式
@@ -34,20 +34,19 @@ int main() {
 	y = uniform_int_distribution<>(1, 10)(rand);
 	int z = uniform_int_distribution<>(1, 10)(rand);
 	int w = uniform_int_distribution<>(1, 10)(rand);
-	questions[2].s = to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" + to_string(w);
+	questions[2].s = to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" + to_string(w) + "の答えは？";
 	questions[2].a = x - (y + z);
 
 	//三角形の面積
 	x = uniform_int_distribution<>(1, 10)(rand);
 	y = uniform_int_distribution<>(1, 5)(rand) * 2;
 	questions.push_back({
-		"面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。",
-		x });
+		"面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。",x });
 
 	cout << "[リクルート試験対策クイズ]\n";
 
 	for (const auto& e : questions) {
-		cout << e.s << "の答えは？\n";
+		cout << e.s << "\n";
 		int answer;
 		cin >> answer;
 		if (answer == e.a) {
