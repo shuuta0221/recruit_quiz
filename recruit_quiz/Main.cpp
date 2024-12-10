78eb8a3035ee65c3b2d1cc2b44798a7e6ed678fe
しゅうた <takumi.onoe@gmail.com>
2024-12-10 18:36:20 +0900

    地理の問題(特徴から都道府県)を作成

--- recruit_quiz/Main.cpp
+++ recruit_quiz/Main.cpp
@@ -2,6 +2,7 @@
 #include "exam_japanese.h"
 #include "exam_english.h"
 #include "exam_science.h"
+#include "exam_geography.h"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -100,7 +101,7 @@ int main() {
 
 	cout << "[リクルート試験対策クイズ]\n";
 
-	cout << "教科を選んでください\n1=数学\n2=国語\n3=英語\n4=理科\n";
+	cout << "教科を選んでください\n1=数学\n2=国語\n3=英語\n4=理科\n5=地理\n";
 	int subject;
 	cin >> subject;
 	if (subject == 2) {
@@ -119,6 +120,8 @@ int main() {
 		questions.insert(questions.end(), phraseExam.begin(), phraseExam.end());
 	} else if (subject == 4) {
 		questions = CreatePhysicsExam();
+	} else if (subject == 5) {
+		questions = CreatePrefecturesExam();
 	}
 
 	for (const auto& e : questions) {
