f960ed6e6d6740d6f1a6cfe4f2273998b63c2a08
しゅうた <takumi.onoe@gmail.com>
2024-12-17 19:04:20 +0900

    半角全角関係なく答えを入力できるように

--- recruit_quiz/Main.cpp
+++ recruit_quiz/Main.cpp
@@ -4,6 +4,7 @@
 #include "exam_science.h"
 #include "exam_geography.h"
 #include "exam_politics.h"
+#include "utility.h"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -131,6 +132,13 @@ int main() {
 		cout << e.s << "\n";
 		string answer;
 		cin >> answer;
+
+		const string ascii = ConvertSjisNumberToAscii(answer);
+		//•ÏŠ·‚ª¬Œ÷‚µ‚½ê‡‚Í•¶š—ñ‚ğ’u‚«Š·‚¦‚é
+		if (!ascii.empty()) {
+			answer = ascii;
+		}
+
 		if (answer == e.a) {
 			cout << "³‰ğI\n";
 		}
