b6da0d1f3d8e84679d94e0a0351be78d41f80f9e
しゅうた <takumi.onoe@gmail.com>
2024-12-17 18:36:20 +0900

    政治の試験の問題を作成

--- recruit_quiz/Main.cpp
+++ recruit_quiz/Main.cpp
@@ -3,6 +3,7 @@
 #include "exam_english.h"
 #include "exam_science.h"
 #include "exam_geography.h"
+#include "exam_politics.h"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -101,7 +102,7 @@ int main() {
 
 	cout << "[リクルート試験対策クイズ]\n";
 
-	cout << "教科を選んでください\n1=数学\n2=国語\n3=英語\n4=理科\n5=地理\n";
+	cout << "教科を選んでください\n1=数学\n2=国語\n3=英語\n4=理科\n5=地理\n6=政治\n";
 	int subject;
 	cin >> subject;
 	if (subject == 2) {
@@ -122,6 +123,8 @@ int main() {
 		questions = CreatePhysicsExam();
 	} else if (subject == 5) {
 		questions = CreatePrefecturesExam();
+	} else if (subject == 6) {
+		questions = CreatePoliticsExam();
 	}
 
 	for (const auto& e : questions) {
